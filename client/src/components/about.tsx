17e31cad170c638feec9623e78c961c1fb4cb718
Will Woodward <will@woodwardweb.com>
2024-10-01 18:57:11 +0100

    fixed mountains on small screens

--- client/src/components/about.tsx
+++ client/src/components/about.tsx
@@ -9,14 +9,25 @@ import { MountainGraphic } from "@/components/graphics"
 export function About() {
   const ref = React.useRef(null)
   const isInView = useInView(ref, { once: true })
+  const [isMobile, setIsMobile] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    handleResize()
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   return (
     <div>
       <div ref={ref} className="w-full h-full absolute">
         <motion.div
           className="absolute flex justify-center top-[-7rem] md:top-[-10rem] lg:top-[-13rem] z-30 pointer-events-none w-full"
-          style={{ transformOrigin: "50% 500rem" }}
-          animate={{ rotate: isInView ? 0 : -30 }}
+          style={{ transformOrigin: "50% 300rem" }}
+          animate={{ rotate: isMobile ? 0 : isInView ? 0 : -30 }}
           transition={{ duration: 1.5, ease: "easeInOut" }}
         >
           <MountainGraphic className="relative h-[12rem] md:h-[16rem] lg:h-[20rem]" />
